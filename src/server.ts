05f6560df1833cbfc694d27797879d54156fb75d
Christian Lee <christianjslee@gmail.com>
2023-10-29 18:44:46 -0400

    Switch to Docker/Heroku

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { schema } from "./schema";
 import { SERVER_PORT } from "./constants";
 import { createYoga } from "graphql-yoga";
 
-const yoga = createYoga({ schema, logging: "error" });
+const yoga = createYoga({ schema, logging: "debug" });
 
 export default {
   port: SERVER_PORT,
